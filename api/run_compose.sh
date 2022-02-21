349fd0e0d02ee813eb789fdeff0bacf223ebd4ff
Luiz Felipe <lfelipecarvalhedo@gmail.com>
2022-02-21 15:39:01 -0300

    test

--- api/run_compose.sh
+++ api/run_compose.sh
@@ -2,10 +2,10 @@
 case $1 in
   "test")
     echo REACT_APP_BASE_URL="backend" > frontend/.env
-    docker-compose --profile tests build && docker-compose --profile tests up --exit-code-from tests tests && bash -c rm frontend/.env
+    docker-compose --profile tests build && docker-compose --profile tests up --exit-code-from tests tests
     ;;
   *)
     echo REACT_APP_BASE_URL="localhost" > frontend/.env
-    docker-compose build && docker-compose up && bash -c rm frontend/.env
+    docker-compose build && docker-compose up
     ;;
 esac
