01c21dbef7d968d9556ccebb80ba45ea9fb62691
Luiz Felipe <lfelipecarvalhedo@gmail.com>
2022-02-21 16:07:06 -0300

    add: terminal for every docker container

--- api/run_compose.sh
+++ api/run_compose.sh
@@ -2,7 +2,7 @@
 case $1 in
   "test")
     echo REACT_APP_BASE_URL="backend" > frontend/.env
-    docker-compose --profile tests build && docker-compose --profile tests up --exit-code-from tests tests
+    docker-compose --profile tests build && docker-compose --profile tests up --exit-code-from tests
     ;;
   *)
     echo REACT_APP_BASE_URL="localhost" > frontend/.env
